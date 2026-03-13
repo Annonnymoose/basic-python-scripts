04967682b60961da988ea2e5661d7964bf5fa100
Annonnymoose <karansoni02749@gmail.com>
2026-03-13 14:34:30 +0530

    Updated API fetcher to continuously log ISS coordinates to a CSV file

--- api_fetcher.py
+++ api_fetcher.py
@@ -1,27 +1,44 @@
 import requests
-api_url = "http://api.open-notify.org/iss-now.json"
+import time, csv
 
-print("Fetching Live Data....")
+api_url = "http://api.open-notify.org/iss-now.json"
+filename = "iss_tracker.csv"
+
+with open(filename, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['timestamp', 'Latitude', 'Longitude'])
+
+print(f"Tracking started! Saving data to {filename}. Press Ctrl+C to stop.")
+
 
 try:
-    response = requests.get(api_url)
+    while True:
+        response = requests.get(api_url)
 
-    if response.status_code == 200:
-        data = response.json()
-        timestamp = data['timestamp']
-        latitude = data['iss_position']['latitude']
-        longitude = data['iss_position']['longitude']
+        if response.status_code == 200:
+            data = response.json()
+            timestamp = data['timestamp']
+            latitude = data['iss_position']['latitude']
+            longitude = data['iss_position']['longitude']
 
-        print("Success")
-        print(f"The ISS is currently Located at: ")
-        print(f"Latitude: {latitude}")
-        print(f"Longitude: {longitude}")
-        print(f"Timestamp: {timestamp}")
+            print("Success")
+            print(f"The ISS is currently Located at: ")
+            print(f"Latitude: {latitude}")
+            print(f"Longitude: {longitude}")
+            print(f"Timestamp: {timestamp}")
 
-    else:
-        print(f"Failed to receive data. Server returned status code: {response.status_code}")
+            with open(filename, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([timestamp, latitude, longitude])
+
+            print(f"Saved coordinates at {timestamp} -> Lat: {latitude}, Lon: {longitude}")
+
+        else:
+            print(f"Failed to receive data. Server returned status code: {response.status_code}")
 
 except requests.exceptions.ConnectionError:
     print("Error: Could not connect to the internet or the API server.")
+except KeyboardInterrupt:
+    print("\nTracking stopped. Your CSV file is ready!")
 except Exception as e:
-    print(f"An unexpected error occurered: {e}")
+    print(f"An unexpected error occurred: {e}")
